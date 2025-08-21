ca204111c03e043502dbc9962d3bfab23b0c7b7f
AbigailAC <Abiuwu@protonmail.com>
2025-08-21 18:37:33 -0400

    feat: Create Aiuthor class, Add Entity Framework support, and Db connection details

---
+++ PurrfectBlog/PurrfectBlog/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PurrfectBlog.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        public string HashedPassword { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ PurrfectBlog/PurrfectBlog/Models/BlogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace PurrfectBlog.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext() : base("DefaultConnection") { }
+        public DbSet<Author> Authors { get; set; }
+    }
+}

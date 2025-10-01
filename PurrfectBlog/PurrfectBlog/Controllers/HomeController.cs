996cb849eff1648f398dabd65d6f1891a78f50fc
AbigailAC <66750060+AbigailAC@users.noreply.github.com>
2025-10-01 00:57:58 -0400

    feat(post): Implement functionality to view posts on home page, posts list, and post details (#8)
    * feat(post): Add controller actions to display posts
    
    * feat(view): Build views for post list, details, and home page
    
    * feat(nav): Add Posts link to main navigation
    
    * refactor: Remove unused using statements and comments
    
    * fix(post): corrects a typo on the error message displayed when post creation author is null
    
    * fix(controller): updated empty XML comments + fixed a typo in Model Error
    
    * fix(controller): updated ViewBag messages
    
    * fix(controller): updated home controller to fix typo

--- PurrfectBlog/PurrfectBlog/Controllers/HomeController.cs
+++ PurrfectBlog/PurrfectBlog/Controllers/HomeController.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PurrfectBlog.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace PurrfectBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BlogDbContext _context = new BlogDbContext();
+
+        /// Displays the home page with the three most recent blog posts.
+        /// </summary>
+        /// <returns>A view showing the recent blog posts.</returns>
         public ActionResult Index()
         {
-            return View();
+            var recentPosts = _context.Posts
+                          .Include(p => p.Author)
+                          .OrderByDescending(p => p.CreationDate)
+                          .Take(3)
+                          .ToList();
+
+            return View(recentPosts);
         }
 
+        /// Displays the About page for the application.
+        /// </summary>
+        /// <returns>The About view.</returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +33,26 @@ namespace PurrfectBlog.Controllers
             return View();
         }
 
+        /// Displays the contact page.
+        /// </summary>
+        /// <returns>The contact view.</returns>
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your Contact page.";
 
             return View();
+        }
+
+        /// Releases the unmanaged resources used by the HomeController and optionally disposes of the managed resources.
+        /// </summary>
+        /// <param name="disposing">True to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- PurrfectBlog/PurrfectBlog/Controllers/PostController.cs
+++ PurrfectBlog/PurrfectBlog/Controllers/PostController.cs
@@ -2,18 +2,59 @@
 using PurrfectBlog.Models;
 using System.Web.Mvc;
 using System.Linq;
+using System.Data.Entity;
 
 namespace PurrfectBlog.Controllers
 {
+    /// Controller responsible for managing blog posts, including listing, viewing details, creating posts, and associating posts with authors.
     [Authorize]
     public class PostController : Controller
     {
         private readonly BlogDbContext _context = new BlogDbContext();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var posts = _context.Posts
+                    .Include(p => p.Author)
+                    .OrderByDescending(p => p.CreationDate)
+                    .ToList();
+            return View(posts);
+        }
+
+        /// Displays the details of a specific blog post identified by its ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the blog post to display. If null, a BadRequest result is returned.</param>
+        /// <returns>
+        /// Returns the details view for the specified post if found; otherwise, returns a BadRequest or NotFound result.
+        /// </returns>
+        [AllowAnonymous]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            var post = _context.Posts.Include(p => p.Author).SingleOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
         public ActionResult Create()
         {
             return View();
         }
 
+        /// Handles the creation of a new blog post. If the model is valid and an author is found,
+        /// the post is saved and the user is redirected to the details page. Otherwise, displays validation errors.
+        /// </summary>
+        /// <param name="post">The post data submitted by the user.</param>
+        /// <returns>
+        /// Redirects to the details page of the newly created post if successful; otherwise, returns the view with validation errors.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Post post)
@@ -31,18 +72,20 @@ namespace PurrfectBlog.Controllers
 
                     _context.Posts.Add(post);
                     _context.SaveChanges();
-                    //TODO: implement redirect to the post details page (likely in next step)
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Details", new { id = post.Id });
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Cannot find an author for the current user");
+                    ModelState.AddModelError("", "Oops! An error occurred. Unable to associate post with a valid author. If this continues happening, please contact us");
                 }
                
             }
             return View(post);
         }
 
+        /// Releases the unmanaged resources used by the controller and optionally disposes of the managed resources.
+        /// </summary>
+        /// <param name="disposing">True to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)

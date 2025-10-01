e0fa41d89b024457e731e6522c7054bf7ede64df
AbigailAC <Abiuwu@protonmail.com>
2025-09-30 20:24:28 -0400

    feat(post): Add controller actions to display posts

--- PurrfectBlog/PurrfectBlog/Controllers/HomeController.cs
+++ PurrfectBlog/PurrfectBlog/Controllers/HomeController.cs
@@ -1,30 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PurrfectBlog.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace PurrfectBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BlogDbContext _context = new BlogDbContext();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+            var recentPosts = _context.Posts
+                          .Include(p => p.Author)
+                          .OrderByDescending(p => p.CreationDate)
+                          .Take(3)
+                          .ToList();
+
+            return View(recentPosts);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My application description.";
 
             return View();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My Contact page.";
 
             return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- PurrfectBlog/PurrfectBlog/Controllers/PostController.cs
+++ PurrfectBlog/PurrfectBlog/Controllers/PostController.cs
@@ -2,18 +2,58 @@
 using PurrfectBlog.Models;
 using System.Web.Mvc;
 using System.Linq;
+using System.Data.Entity;
 
 namespace PurrfectBlog.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Authorize]
     public class PostController : Controller
     {
         private readonly BlogDbContext _context = new BlogDbContext();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var posts = _context.Posts
+                    .Include(p => p.Author)
+                    .OrderByDescending(p => p.CreationDate)
+                    .ToList();
+            return View(posts);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            var post = _context.Posts.Include(p => p.Author).SingleOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
         public ActionResult Create()
         {
             return View();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Post post)
@@ -32,17 +72,21 @@ namespace PurrfectBlog.Controllers
                     _context.Posts.Add(post);
                     _context.SaveChanges();
                     //TODO: implement redirect to the post details page (likely in next step)
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Details", new { id = post.Id });
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Cannot find an author for the current user");
+                    ModelState.AddModelError("", "Oops! An error occured. Unable to associate post with a valid author. If this continues happening, please contact ");
                 }
                
             }
             return View(post);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
